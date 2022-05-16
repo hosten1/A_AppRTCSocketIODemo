5f4c9ffe5d73b4a85eddd92184365b2265f31b5a
luoyongmeng <hosten_lym@163.com>
2022-05-16 12:15:22 +0800

    socket 忽略证书

--- JavaAppRTCSocketIODemo/app/src/main/java/com/example/Signal/SocketManager.java
+++ JavaAppRTCSocketIODemo/app/src/main/java/com/example/Signal/SocketManager.java
@@ -2,11 +2,89 @@ package com.example.Signal;
 
 import android.util.Log;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.net.URISyntaxException;
+import java.security.SecureRandom;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+import java.util.Map;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 import io.socket.client.IO;
 import io.socket.client.Socket;
 import io.socket.emitter.Emitter;
+import okhttp3.Call;
+import okhttp3.OkHttpClient;
+import okhttp3.WebSocket;
+
+class SSLSocketClient {
+
+public static  X509TrustManager myX509TrustManager = new X509TrustManager() {
+    @Override
+    public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+
+    }
+
+    @Override
+    public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+
+    }
+
+    @Override
+    public X509Certificate[] getAcceptedIssuers() {
+        return new X509Certificate[0];
+    }
+};
+public  static SSLSocketFactory getSSLSocketFactory(){
+    try {
+
+            SSLContext sslContext = SSLContext.getInstance("SSL");
+            sslContext.init(null, getTrustManager(), new SecureRandom());
+             return sslContext.getSocketFactory();
+
+        } catch (Exception e) {
+
+                throw new RuntimeException(e);
+
+         }
+}
+    //获取TrustManager
+
+private static TrustManager[] getTrustManager() {
+
+    TrustManager[] trustAllCerts = new TrustManager[]{
+
+     myX509TrustManager
+
+ };
+
+return trustAllCerts;
+
+}
+
+ //获取HostnameVerifier
+ public static HostnameVerifier getHostnameVerifier() {
+
+        HostnameVerifier hostnameVerifier = new HostnameVerifier() {
+            @Override
+            public boolean verify(String hostname, SSLSession session) {
+                return true;
+            }
+        };
+        return hostnameVerifier;
+
+}
+
+
+}
 
 public class SocketManager {
     private static  final String TAG = "SocketManager";
@@ -14,6 +92,19 @@ public class SocketManager {
     private  static SocketManager singleInstace;
     private Socket mSocket;
     private  String mRoomName;
+    private  OnSocketEventListener mListener;
+
+    public interface OnSocketEventListener {
+        void onConnected();
+        void onConnecting();
+        void onDisconnected();
+        void onUserJoined(String roomName, String userID);
+        void onUserLeaved(String roomName, String userID);
+        void onRemoteUserJoined(String roomName);
+        void onRemoteUserLeaved(String roomName, String userID);
+        void onRoomFull(String roomName, String userID);
+        void onMessage(JSONObject message);
+    }
     //实现单例
     public  static SocketManager getInstance(){
         synchronized (SocketManager.class){
@@ -23,10 +114,30 @@ public class SocketManager {
         }
         return singleInstace;
     }
+    // 设置消息监听对象
+    public void setListener(final OnSocketEventListener listener) {
+        mListener = listener;
+    }
     public  void  joinRoom(String serverAdd , String roomName) throws URISyntaxException {
         Log.i(TAG,"join() serverAddr = "+serverAdd +" roomName = "+roomName);
         try {
-            mSocket =  IO.socket(serverAdd);
+
+            OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                    .hostnameVerifier(SSLSocketClient.getHostnameVerifier())
+                    .sslSocketFactory(SSLSocketClient.getSSLSocketFactory(),SSLSocketClient.myX509TrustManager)
+                    .build();
+
+//default settings for all sockets
+
+            IO.setDefaultOkHttpWebSocketFactory(okHttpClient);
+
+            IO.setDefaultOkHttpCallFactory(okHttpClient);
+            IO.Options opt = new  IO.Options();
+            opt.secure = true;
+            opt.callFactory=okHttpClient;
+            opt.webSocketFactory=okHttpClient;
+
+            mSocket =  IO.socket(serverAdd,opt);
             mSocket.connect();
         }catch (URISyntaxException e){
             e.printStackTrace();
@@ -37,24 +148,187 @@ public class SocketManager {
             Log.e(TAG,"初始化socket 失败");
 
         }
-        mSocket.on("joined", new Emitter.Listener() {
+        mSocket.on(Socket.EVENT_CONNECT_ERROR, new Emitter.Listener() {
             @Override
             public void call(Object... args) {
 
+                Log.e(TAG, "onConnectError: " + args);
+            }
+        });
+
+        mSocket.on(Socket.EVENT_ERROR, new Emitter.Listener() {
+            @Override
+            public void call(Object... args) {
+
+                Log.e(TAG, "onError: " + args);
+            }
+        });
+
+        mSocket.on(Socket.EVENT_CONNECT, new Emitter.Listener() {
+            @Override
+            public void call(Object... args) {
+                String sessionId = mSocket.id();
+                Log.i(TAG, "onConnected");
+                if (mListener != null) {
+                    mListener.onConnected();
+                }
+                mSocket.emit("join",roomName);
+            }
+        });
+
+        mSocket.on(Socket.EVENT_CONNECTING, new Emitter.Listener() {
+            @Override
+            public void call(Object... args) {
+                Log.i(TAG, "onConnecting");
+                if (mListener != null) {
+                    mListener.onConnecting();
+                }
+            }
+        });
+
+        mSocket.on(Socket.EVENT_DISCONNECT, new Emitter.Listener() {
+            @Override
+            public void call(Object... args) {
+                Log.i(TAG, "onDisconnected");
+                if (mListener != null) {
+                    mListener.onDisconnected();
+                }
             }
         });
         mSocket.on("joined", new Emitter.Listener() {
+            @Override
+            public void call(Object... args) {
+                try {
+
+
+                    JSONObject msg = (JSONObject) args[0];
+                    String roomName = msg.getString("room");
+                    String userId = msg.getString("id");
+                    Log.e(TAG, "Received joined msg = " + args.toString());
+                    if (mListener != null) {
+                        mListener.onUserJoined(roomName, userId);
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        mSocket.on("leaved", new Emitter.Listener() {
+            @Override
+            public void call(Object... args) {
+                try {
+
+
+                    JSONObject msg = (JSONObject) args[0];
+                    String roomName = msg.getString("room");
+                    String userId = msg.getString("id");
+                    Log.e(TAG, "Received joined msg = " + args.toString());
+                    if (mListener != null) {
+                        mListener.onUserLeaved(roomName, userId);
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                Log.i(TAG, "onUserLeaved, room:" + roomName + "uid:" + userId);
+            }
+        });
+
+        mSocket.on("otherjoin", new Emitter.Listener() {
+
+            @Override
+            public void call(Object... args) {
+                try {
+
+
+                    JSONObject msg = (JSONObject) args[0];
+                    String roomName = msg.getString("room");
+                    String userId = msg.getString("id");
+                    Log.e(TAG, "Received joined msg = " + args.toString());
+                    if (mListener != null) {
+                        mListener.onRemoteUserJoined(roomName);
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                Log.i(TAG, "onRemoteUserJoined, room:" + roomName + "uid:" + userId);
+            }
+        });
+
+        mSocket.on("bye", new Emitter.Listener() {
             @Override
             public void call(Object... args) {
                 String roomName = (String) args[0];
-                String userID = (String) args[1];
-                Log.e(TAG,"Received joined msg = " + args.toString());
+                String userId = (String) args[1];
+                if (mListener != null) {
+                    mListener.onRemoteUserLeaved(roomName, userId);
+                }
+                Log.i(TAG, "onRemoteUserLeaved, room:" + roomName + "uid:" + userId);
+
+            }
+        });
+
+        mSocket.on("full", new Emitter.Listener() {
+            @Override
+            public void call(Object... args) {
+
+                //释放资源
+                mSocket.disconnect();
+                mSocket.close();
+                mSocket = null;
+
+                String roomName = (String) args[0];
+                String userId = (String) args[1];
+
+                if (mListener != null) {
+                    mListener.onRoomFull(roomName, userId);
+                }
+
+                Log.i(TAG, "onRoomFull, room:" + roomName + "uid:" + userId);
+
+            }
+        });
+
+        mSocket.on("message", new Emitter.Listener() {
+            @Override
+            public void call(Object... args) {
+                try {
+                    JSONObject msgT = (JSONObject) args[0];
+                    String id = msgT.getString("id");
+                    Log.e(TAG, "Received joined msg = " + args.toString());
+                    JSONObject msg = msgT.getJSONObject("sdp");
+
+                    if (mListener != null) {
+                        mListener.onMessage(msg);
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                Log.i(TAG, "onMessage, room:" + roomName + "data:" + msg);
 
             }
         });
 
 
-mSocket.emit("join",roomName);
+    }
+    public  void  sendMsg(JSONObject message){
+        Log.i(TAG, "broadcast: " + message);
+        if (mSocket == null) {
+            return;
+        }
+        mSocket.emit("message", mRoomName, message);
+    }
+
+    public  void  close(){
+        Log.i(TAG, "leaveRoom: " + mRoomName);
+        if (mSocket != null)
+            mSocket.emit("leave", mRoomName);
+            mSocket.close();
+            mSocket = null;
     }
 
 }
--- JavaAppRTCSocketIODemo/app/src/main/java/com/example/javaapprtcsocketiodemo/CallActivity.java
+++ JavaAppRTCSocketIODemo/app/src/main/java/com/example/javaapprtcsocketiodemo/CallActivity.java
@@ -2,13 +2,353 @@ package com.example.javaapprtcsocketiodemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.Signal.SocketManager;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.webrtc.AudioSource;
+import org.webrtc.AudioTrack;
+import org.webrtc.Camera1Enumerator;
+import org.webrtc.Camera2Enumerator;
+import org.webrtc.CameraEnumerator;
+import org.webrtc.DefaultVideoDecoderFactory;
+import org.webrtc.DefaultVideoEncoderFactory;
+import org.webrtc.EglBase;
+import org.webrtc.IceCandidate;
+import org.webrtc.Logging;
+import org.webrtc.MediaConstraints;
+import org.webrtc.PeerConnection;
+import org.webrtc.PeerConnectionFactory;
+import org.webrtc.RendererCommon;
+import org.webrtc.SdpObserver;
+import org.webrtc.SessionDescription;
+import org.webrtc.SurfaceTextureHelper;
+import org.webrtc.SurfaceViewRenderer;
+import org.webrtc.VideoCapturer;
+import org.webrtc.VideoDecoderFactory;
+import org.webrtc.VideoEncoderFactory;
+import org.webrtc.VideoSource;
+import org.webrtc.VideoTrack;
+
+import java.net.URISyntaxException;
 
 public class CallActivity extends AppCompatActivity {
-//    private sufa
+    private static final int VIDEO_RESOLUTION_WIDTH = 1280;
+    private static final int VIDEO_RESOLUTION_HEIGHT = 720;
+    private static final int VIDEO_FPS = 30;
+
+    private String mState = "init";
+
+    private TextView mLogcatView;
+
+    private static final String TAG = "CallActivity";
+
+    public static final String VIDEO_TRACK_ID = "ARDAMSv0";//"";
+    public static final String AUDIO_TRACK_ID = "ARDAMSa0";//"";
+
+    //用于数据传输
+    private PeerConnection mPeerConnection;
+    private PeerConnectionFactory mPeerConnectionFactory;
+
+    //OpenGL ES
+    private EglBase mRootEglBase;
+    //纹理渲染
+    private SurfaceTextureHelper mSurfaceTextureHelper;
+
+    //继承自 surface view
+    private SurfaceViewRenderer mLocalSurfaceView;
+    private SurfaceViewRenderer mRemoteSurfaceView;
+
+    private VideoTrack mVideoTrack;
+    private AudioTrack mAudioTrack;
+
+    private VideoCapturer mVideoCapturer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_call);
+        mLogcatView = findViewById(R.id.logShowTV);
+
+        mRootEglBase = EglBase.create();
+
+        mLocalSurfaceView = findViewById(R.id.LocalViewRender);
+        mRemoteSurfaceView = findViewById(R.id.remoteViewRender);
+
+        mLocalSurfaceView.init(mRootEglBase.getEglBaseContext(), null);
+        mLocalSurfaceView.setScalingType(RendererCommon.ScalingType.SCALE_ASPECT_FILL);
+        mLocalSurfaceView.setMirror(true);
+        mLocalSurfaceView.setEnableHardwareScaler(false /* enabled */);
+
+        mRemoteSurfaceView.init(mRootEglBase.getEglBaseContext(), null);
+        mRemoteSurfaceView.setScalingType(RendererCommon.ScalingType.SCALE_ASPECT_FILL);
+        mRemoteSurfaceView.setMirror(true);
+        mRemoteSurfaceView.setEnableHardwareScaler(true /* enabled */);
+        mRemoteSurfaceView.setZOrderMediaOverlay(true);
+
+        //创建 factory， pc是从factory里获得的
+        mPeerConnectionFactory = createPeerConnectionFactory(this);
+
+        // NOTE: this _must_ happen while PeerConnectionFactory is alive!
+        Logging.enableLogToDebugOutput(Logging.Severity.LS_VERBOSE);
+
+        mVideoCapturer = createVideoCapturer();
+
+        mSurfaceTextureHelper = SurfaceTextureHelper.create("CaptureThread", mRootEglBase.getEglBaseContext());
+        VideoSource videoSource = mPeerConnectionFactory.createVideoSource(false);
+        mVideoCapturer.initialize(mSurfaceTextureHelper, getApplicationContext(), videoSource.getCapturerObserver());
+
+        mVideoTrack = mPeerConnectionFactory.createVideoTrack(VIDEO_TRACK_ID, videoSource);
+        mVideoTrack.setEnabled(true);
+        mVideoTrack.addSink(mLocalSurfaceView);
+
+        AudioSource audioSource = mPeerConnectionFactory.createAudioSource(new MediaConstraints());
+        mAudioTrack = mPeerConnectionFactory.createAudioTrack(AUDIO_TRACK_ID, audioSource);
+        mAudioTrack.setEnabled(true);
+
+
+        SocketManager.getInstance().setListener(mOnSignalEventListener);
+
+        String serverAddr = getIntent().getStringExtra("ServerAddr");
+        String roomName = getIntent().getStringExtra("RoomName");
+        try {
+            SocketManager.getInstance().joinRoom(serverAddr, roomName);
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+    }
+    public static class localeSdpObserver implements SdpObserver {
+        @Override
+        public void onCreateSuccess(SessionDescription sessionDescription) {
+            Log.i(TAG, "SdpObserver: onCreateSuccess !");
+        }
+
+        @Override
+        public void onSetSuccess() {
+            Log.i(TAG, "SdpObserver: onSetSuccess");
+        }
+
+        @Override
+        public void onCreateFailure(String msg) {
+            Log.e(TAG, "SdpObserver onCreateFailure: " + msg);
+        }
+
+        @Override
+        public void onSetFailure(String msg) {
+
+            Log.e(TAG, "SdpObserver onSetFailure: " + msg);
+        }
+    }
+    private VideoCapturer createVideoCapturer() {
+        if (Camera2Enumerator.isSupported(this)) {
+            return createCameraCapturer(new Camera2Enumerator(this));
+        } else {
+            return createCameraCapturer(new Camera1Enumerator(true));
+        }
+    }
+
+    private VideoCapturer createCameraCapturer(CameraEnumerator enumerator) {
+        final String[] deviceNames = enumerator.getDeviceNames();
+
+        // First, try to find front facing camera
+        Log.d(TAG, "Looking for front facing cameras.");
+        for (String deviceName : deviceNames) {
+            if (enumerator.isFrontFacing(deviceName)) {
+                Logging.d(TAG, "Creating front facing camera capturer.");
+                VideoCapturer videoCapturer = enumerator.createCapturer(deviceName, null);
+                if (videoCapturer != null) {
+                    return videoCapturer;
+                }
+            }
+        }
+
+        // Front facing camera not found, try something else
+        Log.d(TAG, "Looking for other cameras.");
+        for (String deviceName : deviceNames) {
+            if (!enumerator.isFrontFacing(deviceName)) {
+                Logging.d(TAG, "Creating other camera capturer.");
+                VideoCapturer videoCapturer = enumerator.createCapturer(deviceName, null);
+                if (videoCapturer != null) {
+                    return videoCapturer;
+                }
+            }
+        }
+        return null;
+    }
+
+    private PeerConnectionFactory createPeerConnectionFactory(Context context) {
+        final VideoEncoderFactory encoderFactory;
+        final VideoDecoderFactory decoderFactory;
+
+        encoderFactory = new DefaultVideoEncoderFactory(
+                mRootEglBase.getEglBaseContext(),
+                false /* enableIntelVp8Encoder */,
+                true);
+        decoderFactory = new DefaultVideoDecoderFactory(mRootEglBase.getEglBaseContext());
+
+        PeerConnectionFactory.initialize(PeerConnectionFactory.InitializationOptions.builder(context)
+                .setEnableInternalTracer(true)
+                .createInitializationOptions());
+
+        PeerConnectionFactory.Builder builder = PeerConnectionFactory.builder()
+                .setVideoEncoderFactory(encoderFactory)
+                .setVideoDecoderFactory(decoderFactory);
+        builder.setOptions(null);
+
+        return builder.createPeerConnectionFactory();
+    }
+    
+    private void updateCallState(boolean idle) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                if (idle) {
+                    mRemoteSurfaceView.setVisibility(View.GONE);
+                } else {
+                    mRemoteSurfaceView.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+    }
+    private void hangup() {
+        printfToLogCatUtils("Hangup Call, Wait ...");
+        if (mPeerConnection == null) {
+            return;
+        }
+        mPeerConnection.close();
+        mPeerConnection = null;
+        printfToLogCatUtils("Hangup Done.");
+        updateCallState(true);
+    }
+    // 设置socket消息监听
+    private  SocketManager.OnSocketEventListener mOnSignalEventListener = new SocketManager.OnSocketEventListener() {
+        @Override
+        public void onConnected() {
+
+        }
+
+        @Override
+        public void onConnecting() {
+
+        }
+
+        @Override
+        public void onDisconnected() {
+
+        }
+
+        @Override
+        public void onUserJoined(String roomName, String userID) {
+
+        }
+
+        @Override
+        public void onUserLeaved(String roomName, String userID) {
+
+        }
+
+        @Override
+        public void onRemoteUserJoined(String roomName) {
+
+        }
+
+        @Override
+        public void onRemoteUserLeaved(String roomName, String userID) {
+
+        }
+
+        @Override
+        public void onRoomFull(String roomName, String userID) {
+
+        }
+
+        @Override
+        public void onMessage(JSONObject message) {
+            Log.i(TAG, "onMessage: " + message);
+
+            try {
+                String type = message.getString("type");
+                if (type.equals("offer")) {
+                    onRemoteOfferReceived(message);
+                }else if(type.equals("answer")) {
+                    onRemoteAnswerReceived(message);
+                }else if(type.equals("candidate")) {
+                    onRemoteCandidateReceived(message);
+                }else{
+                    Log.w(TAG, "the type is invalid: " + type);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+        private void onRemoteOfferReceived(JSONObject message) {
+            printfToLogCatUtils("Receive Remote Call ...");
+
+            if (mPeerConnection == null) {
+//                mPeerConnection = createPeerConnection();
+            }
+
+            try {
+                String description = message.getString("sdp");
+                mPeerConnection.setRemoteDescription(
+                        new localeSdpObserver(),
+                        new SessionDescription(
+                                SessionDescription.Type.OFFER,
+                                description));
+//                doAnswerCall();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+        private void onRemoteAnswerReceived(JSONObject message) {
+            printfToLogCatUtils("Receive Remote Answer ...");
+            try {
+                String description = message.getString("sdp");
+                mPeerConnection.setRemoteDescription(
+                        new localeSdpObserver(),
+                        new SessionDescription(
+                                SessionDescription.Type.ANSWER,
+                                description));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            updateCallState(false);
+        }
+
+        private void onRemoteCandidateReceived(JSONObject message) {
+            printfToLogCatUtils("Receive Remote Candidate ...");
+            try {
+                IceCandidate remoteIceCandidate =
+                        new IceCandidate(message.getString("id"),
+                                message.getInt("label"),
+                                message.getString("candidate"));
+
+                mPeerConnection.addIceCandidate(remoteIceCandidate);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+        private void onRemoteHangup() {
+            printfToLogCatUtils("Receive Remote Hangup Event ...");
+            hangup();
+        }
+    };
+    private void printfToLogCatUtils(String msg) {
+        Log.i(TAG, msg);
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                String output = mLogcatView.getText() + "\n" + msg;
+                mLogcatView.setText(output);
+            }
+        });
     }
 }
